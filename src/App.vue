bcbaedc67d08392440ee3e30e4b274b5e4e927bd
JoeyDon <Joey.Don0905@gmail.com>
2019-07-16 23:11:37 +1000

    console log up and down

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,12 @@
     <div id="console"></div>
       <video autoplay playsinline muted id="webcam" width="600" height="600"></video>
     <div>
-      <button id="class-a">Add A</button>
-      <button id="class-b">Add B</button>
-      <button id="class-c">Add C</button>
+      <button id="class-a">Add A Set</button>
+      <button id="class-b">Add B Set</button>
+    </div>
+    <div id="longsheet">
+    a
+
     </div>
   </div>
 </template>
@@ -14,81 +17,17 @@
 import JoeyLib from "./lib/JoeyLib"
 
 export default {
-  name: 'app',
+  data: () => ({
+    instance: null
+  }),
+
+
   mounted(){
-    let net;
-    //6. Run MobileNet Inference in Browser through Webcam images
-    const webcamElement = document.getElementById('webcam');
-    //7. Add a custom classifier on top of the MobileNet predictions
-    const classifier = knnClassifier.create();
+    this.instance = new JoeyLib('webcam','class-a','class-b')
+    this.instance.init()
+  },
 
-
-    async function setupWebcam() {
-      return new Promise((resolve, reject) => {
-        const navigatorAny = navigator;
-        navigator.getUserMedia = navigator.getUserMedia ||
-            navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
-            navigatorAny.msGetUserMedia;
-        if (navigator.getUserMedia) {
-          navigator.getUserMedia({video: true},
-            stream => {
-              webcamElement.srcObject = stream;
-              webcamElement.addEventListener('loadeddata',  () => resolve(), false);
-            },
-            error => reject());
-        } else {
-          reject();
-        }
-      });
-    }
-
-    async function app() {
-      console.log('Loading mobilenet..');
-
-      // Load the model.
-      net = await mobilenet.load();
-      console.log('Sucessfully loaded model');
-
-      await setupWebcam();
-
-      // Reads an image from the webcam and associates it with a specific class
-      // index.
-      const addExample = classId => {
-        // Get the intermediate activation of MobileNet 'conv_preds' and pass that
-        // to the KNN classifier.
-        const activation = net.infer(webcamElement, 'conv_preds');
-
-        // Pass the intermediate activation to the classifier.
-        classifier.addExample(activation, classId);
-      };
-
-      // When clicking a button, add an example for that class.
-      document.getElementById('class-a').addEventListener('click', () => addExample(0));
-      document.getElementById('class-b').addEventListener('click', () => addExample(1));
-      document.getElementById('class-c').addEventListener('click', () => addExample(2));
-
-      while (true) {
-        if (classifier.getNumClasses() > 0) {
-          // Get the activation from mobilenet from the webcam.
-          const activation = net.infer(webcamElement, 'conv_preds');
-          // Get the most likely class and confidences from the classifier module.
-          const result = await classifier.predictClass(activation);
-
-          console.log(result)
-          const classes = ['A', 'B', 'C'];
-          document.getElementById('console').innerText = `
-            prediction: ${classes[result.classIndex]}\n
-            probability: ${result.confidences[result.classIndex]}
-          `;
-        }
-
-        await tf.nextFrame();
-      }
-    }
-    var newInstance = new JoeyLib('aa','output22')
-    newInstance.test()
-    //app();
-  }
+  
 }
 </script>
 
@@ -100,5 +39,9 @@ export default {
   text-align: center;
 
   margin-top: 60px;
+}
+
+#longsheet{
+  height: 5000px;
 }
 </style>
--- src/lib/JoeyLib.js
+++ src/lib/JoeyLib.js
@@ -1,10 +1,102 @@
 export default class JoeyLib{
-    constructor(domElement, output){
-        this.domElement = domElement;
-        this.output = output;
+    constructor(webcamElement, btnElement1, btnElement2){
+        this.btnElement1 = btnElement1;
+        this.btnElement2 = btnElement2;
+        
+        this.webcamElement = document.getElementById(webcamElement);
+        this.classifier = knnClassifier.create();
+
+        this.net;
+        
     }
+
+    async setupWebcam() {
+        return new Promise((resolve, reject) => {
+          const navigatorAny = navigator;
+          navigator.getUserMedia = navigator.getUserMedia ||
+              navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+              navigatorAny.msGetUserMedia;
+          if (navigator.getUserMedia) {
+            navigator.getUserMedia({video: true},
+              stream => {
+                this.webcamElement.srcObject = stream;
+                this.webcamElement.addEventListener('loadeddata',  () => resolve(), false);
+              },
+              error => reject());
+          } else {
+            reject();
+          }
+        });
+      }
+
+      async init() {
+        console.log('Loading mobilenet..');
+  
+        // Load the model.
+        this.net = await mobilenet.load();
+        console.log('Sucessfully loaded model');
+  
+        await this.setupWebcam();
+  
+        // Reads an image from the webcam and associates it with a specific class
+        // index.
+        const addExample = classId => {
+          // Get the intermediate activation of MobileNet 'conv_preds' and pass that
+          // to the KNN classifier.
+          const activation = this.net.infer(this.webcamElement, 'conv_preds');
+  
+          // Pass the intermediate activation to the classifier.
+          this.classifier.addExample(activation, classId);
+        };
+  
+        // When clicking a button, add an example for that class.
+        for (var x = 0; x<20; x++){
+            addExample(0);
+        }
+        document.getElementById(this.btnElement1).addEventListener('click', () => addExample(1));
+        document.getElementById(this.btnElement2).addEventListener('click', () => addExample(2));
+        
+        var elmnt = document.getElementById("longsheet");
+
+        while (true) {
+          if (this.classifier.getNumClasses() > 0) {
+            // Get the activation from mobilenet from the webcam.
+            const activation = this.net.infer(this.webcamElement, 'conv_preds');
+            // Get the most likely class and confidences from the classifier module.
+            this.result = await this.classifier.predictClass(activation);
+  
+            var classes = ['default','up', 'down'];
+            document.getElementById('console').innerText = `
+              prediction: ${classes[this.result.classIndex]}\n
+              probability: ${this.result.confidences[this.result.classIndex]}
+            `;
+          }
+
+          if(classes[this.result.classIndex] === 'up'){
+            elmnt.scrollTop += 1;
+            console.log('uping')
+          }
+          if(classes[this.result.classIndex] === 'down'){
+            elmnt.scrollTop -= 1;
+            console.log('downing')
+          }
+          await tf.nextFrame();
+        }
+      }
 
     test(){
         console.log(this.output)
     }
+
+    getResult(){
+        return this.result
+    }
+
+    //  createTag(){
+    //     var script = document.createElement('script');
+    //     script.type = 'text/javascript';
+    //     script.src = 'https://unpkg.com/@tensorflow/tfjs';
+    //     console.log(script)
+    //     document.body.appendChild(script);
+    // }
 }
